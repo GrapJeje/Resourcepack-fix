e34f4d3251c15b18d1facbac8bde3fc2fc1da9d3
Jason <73070464+GrapJeje@users.noreply.github.com>
2024-06-17 11:15:42 +0200

    Updated main

--- src/main/java/eu/camonetwork/resourcepackfix/Main.java
+++ src/main/java/eu/camonetwork/resourcepackfix/Main.java
@@ -1,5 +1,8 @@
 package eu.camonetwork.resourcepackfix;
 
+import eu.camonetwork.resourcepackfix.Infrastructure.Data.Configs.ConfigManager;
+import eu.camonetwork.resourcepackfix.Infrastructure.Data.Configs.DefaultConfig;
+import eu.camonetwork.resourcepackfix.Infrastructure.Data.Configs.MessagesConfig;
 import eu.camonetwork.resourcepackfix.commands.ResourcepackfixCommand;
 import eu.camonetwork.resourcepackfix.commands.ResourcepackfixTabCompleter;
 import org.bukkit.command.CommandExecutor;
@@ -7,8 +10,35 @@ import org.bukkit.command.PluginCommand;
 import org.bukkit.command.TabCompleter;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public final class Main extends JavaPlugin {
     public static Main instance;
+    public static Manager manager;
+    public static DefaultConfig defaultConfig;
+    public static MessagesConfig messagesConfig;
+    public static List<ConfigManager> configList = new ArrayList<>();
+
+    @Override
+    public void onEnable() {
+        instance = this;
+
+        // Initialize Configs
+        saveDefaultConfig();
+        defaultConfig = new DefaultConfig();
+        messagesConfig = new MessagesConfig();
+
+        ReloadConfigs();
+
+        configList.add(defaultConfig);
+        configList.add(messagesConfig);
+
+        // Register Commands
+        registerCommand("resourcepackfix", new ResourcepackfixCommand(), new ResourcepackfixTabCompleter());
+
+        manager = new Manager(defaultConfig, messagesConfig);
+    }
 
     public void registerCommand(String cmdName, CommandExecutor executor, TabCompleter tabCompleter) {
         PluginCommand command = getServer().getPluginCommand(cmdName);
@@ -20,12 +50,8 @@ public final class Main extends JavaPlugin {
         }
     }
 
-    @Override
-    public void onEnable() {
-        instance = this;
-
-        registerCommand("resourcepackfix", new ResourcepackfixCommand(), new ResourcepackfixTabCompleter());
-
+    public static void ReloadConfigs() {
+        defaultConfig.reload();
+        messagesConfig.reload();
     }
-
 }
